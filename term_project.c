07fbed04e7ac600122c07ae69fe7918a49ba4ca4
cluelaw03 <cluelaw0815@gmail.com>
2026-05-28 22:40:43 +0900

    Add files via upload

---
+++ term_project.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdatomic.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdint.h>
+
+
+int main(int argc, char* argv[]) {
+    
+    return 0;
+}
