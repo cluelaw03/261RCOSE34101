c22aa548cf97bc73376303bd571a8ef5cc635a7e
cluelaw03 <cluelaw0815@gmail.com>
2026-05-29 17:33:53 +0900

    parted2

---
+++ Main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "globals.h"
+#include "scenario.h"
+#include "utils.h"
+#include "scheduler.h"
+
+int main(void) {
+    srand(time(NULL));
+    create_senario();
+
+    
+    return 0;
+}
