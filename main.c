55003cc90b9c8a36162d2c7c84aa334e4e04edd2
cluelaw03 <cluelaw0815@gmail.com>
2026-05-31 01:47:20 +0900

    plused_output_compiled3

--- main.c
+++ main.c
@@ -12,6 +12,18 @@ int main(void) {
     srand(time(NULL));
     create_senario();
 
+    scheduler_6(FCFS);
+    print_gantt(FCFS);
+    scheduler_6(SJF_NP);
+    print_gantt(SJF_NP);
+    scheduler_6(SJF_P);
+    print_gantt(SJF_P);
+    scheduler_6(PRIO_NP);
+    print_gantt(PRIO_NP);
+    scheduler_6(PRIO);
+    print_gantt(PRIO);
+    scheduler_6(RR);
+    print_gantt(RR);
     
     return 0;
 }
